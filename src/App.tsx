df024d61bfd76b3495d46e7f3d530f61fe5a48c9
Octexor <octexor@pm.me>
2022-05-19 21:13:31 +0200

    Add lists

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ import { ChevronDownIcon } from "@chakra-ui/icons"
 import ReactJson from 'react-json-view'
 import exportFromJSON from 'export-from-json'
 import writeXlsxFile from 'write-excel-file'
-import getTokenId, {normalize} from "./helpers/name2id";
+import getTokenId, { normalize } from "./helpers/name2id";
 import parseInput from "./helpers/parse-input";
 
 interface Name2IdMap {
@@ -55,19 +55,24 @@ export const App = () => {
   let [totalCount, setTotalCount] = React.useState(0);
   let [isId2Name, setIsId2Name] = React.useState(false);
   let [refresh, setRefresh] = React.useState(false);
-  const exportTypes = [{name:'CSV', val: 'csv'}, {name:'XLS', val: 'xls'}];
+  const exportTypes = [{ name: 'CSV', val: 'csv' }, { name: 'XLS', val: 'xls' }];
+  const lists = [
+    { name: '999 Club', val: '999' },
+    { name: '10k Club', val: '10k' },
+    { name: '100k Club', val: '100k' }
+  ];
   const maxRendered = 500;
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInput(e.target.value);
   };
   const handleExport = (type: ExportTypes) => {
     let keys;
-    if(isId2Name) {
+    if (isId2Name) {
       keys = Object.keys(result).sort((a, b) => (result[a].localeCompare(result[b])));
     } else {
       keys = Object.keys(result).sort();
     }
-    if(type === "json") {
+    if (type === "json") {
       exportFromJSON({
         data: result,
         fileName: `ens-name-tokenIds-${Date.now()}`,
@@ -75,17 +80,17 @@ export const App = () => {
         replacer: keys
       });
     } else {
-      const dataArray: {name: string, tokenId: string}[] = [];
-        if(isId2Name) {
-          keys.forEach(id => {
-            dataArray.push({tokenId: id, name: result[id]});
-          });
-        } else {
-          keys.forEach(name => {
-            dataArray.push({name, tokenId: result[name]});
+      const dataArray: { name: string, tokenId: string }[] = [];
+      if (isId2Name) {
+        keys.forEach(id => {
+          dataArray.push({ tokenId: id, name: result[id] });
+        });
+      } else {
+        keys.forEach(name => {
+          dataArray.push({ name, tokenId: result[name] });
         });
       }
-      if(type === 'xls') {
+      if (type === 'xls') {
         writeXlsxFile(dataArray, {
           schema: isId2Name ? xlsSchema.reverse() : xlsSchema,
           fileName: `ens-name-tokenIds-${Date.now()}.xlsx`
@@ -100,6 +105,28 @@ export const App = () => {
     }
   };
 
+  const list = (listName: string) => {
+    let content = '';
+    switch (listName) {
+      case '999':
+        for (let i = 0; i < 1000; i++) {
+          content += (i + '').padStart(3, '0') + ' ';
+        }
+        break;
+      case '10k':
+        for (let i = 0; i < 10000; i++) {
+          content += (i + '').padStart(4, '0') + ' ';
+        }
+        break;
+      case '100k':
+        for (let i = 0; i < 100000; i++) {
+          content += (i + '').padStart(5, '0') + ' ';
+        }
+        break;
+    }
+    setInput(content);
+  };
+
   const generate = (id2name?: boolean) => {
     const map: Name2IdMap = {};
     const displayMap: Name2IdMap = {};
@@ -108,25 +135,25 @@ export const App = () => {
     const invalids: string[] = [];
     setIsId2Name(!!id2name);
     names.forEach(name => {
-      if(name.length === 0) return;
+      if (name.length === 0) return;
       let normal;
       try {
         normal = normalize(name);
         let tokenId = getTokenId(normal);
-        if(id2name) {
+        if (id2name) {
           map[tokenId] = name;
         } else {
           map[name] = tokenId;
         }
         count++;
-        if(count <= maxRendered) {
-          if(id2name) {
+        if (count <= maxRendered) {
+          if (id2name) {
             displayMap[tokenId] = map[tokenId];
           } else {
             displayMap[name] = map[name];
           }
         }
-      } catch(e) {
+      } catch (e) {
         invalids.push(name);
       }
     });
@@ -148,7 +175,7 @@ export const App = () => {
   }
 
   React.useEffect(() => {
-    if(refresh) {
+    if (refresh) {
       generate();
       setRefresh(false);
     }
@@ -164,7 +191,7 @@ export const App = () => {
               <Text fontSize="md">
                 List of ENS names separated by <Code>,</Code> <Code>space</Code> or <Code>new line</Code>
               </Text>
-              <Textarea id="input" h="40" value={input} onChange={handleChange}></Textarea>
+              <Textarea id="input" h="40" maxW="xl" w="full" value={input} onChange={handleChange}></Textarea>
             </VStack>
             <HStack>
               <ButtonGroup isAttached>
@@ -176,7 +203,15 @@ export const App = () => {
                   </MenuList>
                 </Menu>
               </ButtonGroup>
-              <ButtonGroup isAttached>
+              <Menu>
+                <MenuButton as={Button} paddingInlineEnd="2" minW="5" rightIcon={<ChevronDownIcon />}>Lists</MenuButton>
+                <MenuList minW="7rem" fontSize="md">
+                  {lists.map(item => (
+                    <MenuItem onClick={() => list(item.val)} key={item.val}>{item.name}</MenuItem>
+                  ))}
+                </MenuList>
+              </Menu>
+              <ButtonGroup isAttached isDisabled={input.length === 0}>
                 <Button onClick={clear}>Clear</Button>
                 <Menu>
                   <MenuButton as={Button} paddingInlineStart="0" paddingInlineEnd="2" minW="5" rightIcon={<ChevronDownIcon />}></MenuButton>
@@ -196,24 +231,24 @@ export const App = () => {
                   </MenuList>
                 </Menu>
               </ButtonGroup>
-              
+
             </HStack>
             {invalidNames.length > 0 && <Stack spacing={1} fontSize="md">
               {invalidNames.slice(0, 5).map(name => (
-              <Alert status='error' borderRadius="10" py="2">
-                <AlertIcon />
-                Invalid name: {name}
-              </Alert>
+                <Alert status='error' borderRadius="10" py="2">
+                  <AlertIcon />
+                  Invalid name: {name}
+                </Alert>
               ))}
               {invalidNames.length > 5 && <Alert status='error' borderRadius="10" py="2">
-                  <AlertIcon />
-                  {invalidNames.length - 5} more invalid name{invalidNames.length > 6 ? 's' : ''}!
-                </Alert>
+                <AlertIcon />
+                {invalidNames.length - 5} more invalid name{invalidNames.length > 6 ? 's' : ''}!
+              </Alert>
               }
             </Stack>
             }
             {totalCount > 0 && <VStack spacing={1} w="full">
-              { totalCount > maxRendered && <Text fontSize="md">
+              {totalCount > maxRendered && <Text fontSize="md">
                 {totalCount} valid names. First {maxRendered} shown below:
               </Text>
               }
@@ -230,26 +265,26 @@ export const App = () => {
                     borderRadius: "5px",
                     wordBreak: "break-all"
                   }}
-                  ></ReactJson>
+                ></ReactJson>
               </Box>
             </VStack>
             }
           </VStack>
         </Grid>
         <Box
-      bg={'gray.900'}
-      color={'gray.200'}>
-      <Container
-        as={Stack}
-        maxW={'6xl'}
-        py={4}
-        direction='row'
-        spacing={4}
-        justify='center'
-        align='center'>
-        <Text fontSize="md">Built by <Link color="teal.500" href="https://twitter.com/Octexor" target="_blank">@Octexor</Link></Text>
-      </Container>
-    </Box>
+          bg={'gray.900'}
+          color={'gray.200'}>
+          <Container
+            as={Stack}
+            maxW={'6xl'}
+            py={4}
+            direction='row'
+            spacing={4}
+            justify='center'
+            align='center'>
+            <Text fontSize="md">Built by <Link color="teal.500" href="https://twitter.com/Octexor" target="_blank">@Octexor</Link></Text>
+          </Container>
+        </Box>
       </Box>
     </ChakraProvider>
   );
