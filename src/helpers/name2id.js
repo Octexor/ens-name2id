90d9668a8323cd5746468fe16f3b0912bee83690
Octexor <octexor@pm.me>
2022-10-17 03:25:39 +0200

    try another fix

--- src/helpers/name2id.js
+++ src/helpers/name2id.js
@@ -5,7 +5,7 @@ const ethers = require('ethers');
 const BigNumber = ethers.BigNumber;
 const utils = ethers.utils;
 
-const getTokenId = (name: string) => {
+const getTokenId = (name) => {
     const labelHash = utils.keccak256(utils.toUtf8Bytes(name));
     return BigNumber.from(labelHash).toString();
 }
