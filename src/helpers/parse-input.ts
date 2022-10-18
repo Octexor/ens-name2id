4f00a32f01efb0b8f6be38b58de3458872c1c5ea
Octexor <octexor@pm.me>
2022-10-18 23:04:26 +0200

    fix parsing

--- src/helpers/parse-input.ts
+++ src/helpers/parse-input.ts
@@ -1,5 +1,5 @@
 const parseInput = (str: string) => {
-    const delimeters = [/, /g, /,/g, / /g, /.eth/ig, /\./g, /\r\n/g, /\n+/g];
+    const delimeters = [/, /g, /,/g, / /g, /\.eth/ig, /\./g, /\r\n/g, /\n+/g];
     let parsedStr = str;
     delimeters.forEach(d => {
         parsedStr = parsedStr.replace(d, '\n');
